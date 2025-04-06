c62e0a8cd6bdab5acbc1723a7848ee9b8a1d1d7b
zameeeeeel <zameeltaj99@gmail.com>
2025-04-07 02:56:21 +0530

    added service page , updated navigation buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../src/pages/Home_page";
+import Services from "./pages/Service_page";
 // import About from './pages/About'; // Future pages
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/Services" element={<Services/>} />
         {/* <Route path="/about" element={<About />} /> */}
       </Routes>
     </Router>
