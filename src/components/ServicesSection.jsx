efdde73723ca95c47a2f58250bef16ef1621e2fc
aura-dev-business <auradevbusiness@gmail.com>
2025-04-14 08:45:18 +0530

    Update ServicesSection.jsx

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -42,7 +42,10 @@ const ServicesSection = () => {
   useEffect(() => {
     const fetchServices = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/services`);
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/services` ,{
+          headers: {
+            'Content-Type': 'application/json'
+          }});
         console.log(response);
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
